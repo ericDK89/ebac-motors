e93cca2dbedd415bd6cbea93ee7b978ab751ed56
ericDK89 <ericthr42@gmail.com>
2023-04-09 20:58:13 -0300

    add: mask on tel input and add function to select car (promotions-list only)

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
   $(".menu").click(function () {
     $("#nav-container").slideToggle();
   });
+
+  $("#tel").mask("(00) 00000-0000", {
+    placeholder: "(99) 01234-5678",
+  });
+
+  $("#promotions-list").on("click", "button", function () {
+    const carName = $(this).data("car");
+    $("#car").val(carName);
+  });
 });
