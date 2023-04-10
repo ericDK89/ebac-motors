781fe97fb1e5038a49684a41d55e4b6f64919754
ericDK89 <ericthr42@gmail.com>
2023-04-10 15:55:56 -0300

    add: validator and scroll on button click

--- script.js
+++ script.js
@@ -12,8 +12,51 @@ $(document).ready(function () {
     placeholder: "(99) 01234-5678",
   });
 
+  $("#emphasis-list").on("click", "button", function () {
+    const carName = $(this).data("car");
+    $("#car").val(carName);
+  });
+
   $("#promotions-list").on("click", "button", function () {
     const carName = $(this).data("car");
     $("#car").val(carName);
+  });
+
+  $("form").validate({
+    rules: {
+      name: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      tel: {
+        required: true,
+      },
+    },
+    messages: {
+      name: "Nome obrigatório",
+      tel: "Telefone obrigatório",
+      email: "Email obrigatório",
+    },
+    submitHandler: function (form) {
+      console.log(form);
+    },
+    invalidHandler: function (e, validator) {
+      let invalidFields = validator.numberOfInvalids();
+      console.log(invalidFields);
+    },
+  });
+
+  $("section button").on("click", function () {
+    const destiny = $("#contact-section");
+
+    $("html").animate(
+      {
+        scrollTop: destiny.offset().top,
+      },
+      500
+    );
   });
 });
